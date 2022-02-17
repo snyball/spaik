0c106dfe9db663f15ad570d7d6b378b9211aa6a1
Jonas Møller <jonas.moller@student.uib.no>
2022-02-17 17:52:52 +0100

    REPL index cleanup

--- js/index.ts
+++ js/index.ts
@@ -2,15 +2,21 @@ import { SpaikRepl } from './spaik-repl';
 export * from './spaik-repl';
 
 let repl = new SpaikRepl(document.getElementById('terminal'));
-repl.send_line('(println "Welcome to the SPAIK REPL!")');
-repl.send_line('(define (fib-r a b n) (println a) (if (<= n 0) a (fib-r b (+ a b) (- n 1))))');
-repl.send_line('(define (fib n) (fib-r 1 1 n))');
-repl.send_line('(fib 7)');
-// repl.send_line("; Try disassembling fib-r with (disassemble 'fib-r)")
 
-window.onload = () => {
-    console.log("Loaded.");
-    repl.fit();
-};
+export function repl_fib_example() {
+    repl.send_line('(println "Welcome to the SPAIK REPL!")');
+    repl.send_line('(define (fib-r a b n) (println a) (if (<= n 0) a (fib-r b (+ a b) (- n 1))))');
+    repl.send_line('(define (fib n) (fib-r 1 1 n))');
+    repl.send_line('(fib 7)');
+}
+
+export function repl_disassemble_example() {
+    repl.send_line("(disassemble 'max)");
+    repl.send_line("(max '(3 2 1 22 3 100 2 3))")
+}
 
 window.onresize = () => { repl.fit() };
+window.onload = () => {
+    repl.fit()
+    repl_fib_example();
+}
