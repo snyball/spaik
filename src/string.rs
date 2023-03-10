770bf2aa2320922eefec18ec72c0a36e58be221f
Jonas Møller <jonas@moesys.no>
2023-03-10 23:21:27 +0100

    update string.rs
    There's clearly something about how the Rust memory allocation functions
    work that I just don't seem to understand.

--- src/string.rs
+++ src/string.rs
@@ -20,38 +20,65 @@ impl RcStr {
         Self::layout_with(self.len as usize)
     }
 
-    pub unsafe fn buffer(&mut self) -> *mut u8 {
+    pub unsafe fn buffer_mut(&mut self) -> *mut u8 {
         (self as *const RcStr as *mut u8).offset(size_of::<RcStr>() as isize)
+    }
+
+    pub unsafe fn buffer(&self) -> *const u8 {
+        (self as *const RcStr as *const u8).offset(size_of::<RcStr>() as isize)
+    }
+}
+
+impl AsRef<str> for RcStr {
+    fn as_ref(&self) -> &str {
+        unsafe {
+            let v = std::slice::from_raw_parts(self.buffer(), self.len as usize);
+            std::str::from_utf8_unchecked(v)
+        }
     }
 }
 
 pub struct StrBuilder {
     s: *mut RcStr,
     sz: usize,
+    orig_sz: usize,
+    sz_mul: usize,
 }
 
 impl StrBuilder {
-    pub fn new(sz: usize) -> Self {
+    pub fn new(mut sz: usize) -> Self {
         unsafe {
+            sz = sz.max(8);
             let layout = RcStr::layout_with(sz);
             let s = alloc(layout) as *mut RcStr;
-            Self { s, sz }
+            (*s).len = 0;
+            (*s).rc = GcRc::default();
+            Self { s, sz, sz_mul: 1, orig_sz: sz }
         }
     }
 
     pub fn push_char(&mut self, c: char) {
         unsafe {
+            const MAX_CHAR_LEN: usize = 4;
+            const MIN_SZ: usize = MAX_CHAR_LEN * 16;
             let len = (*self.s).len as usize;
-            if len + 4 > self.sz {
-                self.sz = (self.sz * 2).min(4);
+            dbg!((*self.s).as_ref());
+            if len + MAX_CHAR_LEN > self.sz {
+                // self.sz = (self.sz * 2).max(MIN_SZ);
+                self.sz_mul *= 2;
+                self.sz = self.sz_mul * RcStr::layout_with(self.orig_sz).size() - size_of::<RcStr>();
+                dbg!(self.sz_mul);
                 self.s = realloc(self.s as *mut u8,
-                                 RcStr::layout_with(self.sz),
-                                 1) as *mut RcStr;
+                                 RcStr::layout_with(self.orig_sz),
+                                 self.sz_mul) as *mut RcStr;
+                println!("Realloc to {}!", self.sz);
             }
+            dbg!((*self.s).as_ref());
             let dst = std::slice::from_raw_parts_mut(
-                (*self.s).buffer().add(len), 4);
+                (*self.s).buffer_mut().add(len), MAX_CHAR_LEN);
             let s = c.encode_utf8(dst);
             (*self.s).len += s.len() as u32;
+            dbg!((*self.s).as_ref());
         }
     }
 
@@ -66,7 +93,7 @@ impl StrBuilder {
                                  RcStr::layout_with(self.sz),
                                  1) as *mut RcStr;
             }
-            memcpy((*self.s).buffer().add(len), s.as_ptr(), len);
+            memcpy((*self.s).buffer_mut().add(len), s.as_ptr(), len);
         }
     }
 
@@ -135,5 +162,31 @@ impl Drop for Str {
                 dealloc(self.0 as *mut u8, (*self.0).layout())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_str() {
+        let mut b = StrBuilder::new(0);
+        b.push_char('a');
+        b.push_char('b');
+        let p = Str(b.done());
+        assert_eq!(p.as_ref(), "ab");
+    }
+
+    #[test]
+    fn build_long_str() {
+        let mut b = StrBuilder::new(0);
+        b.push_char('a');
+        b.push_char('b');
+        for _ in 0..10000 {
+            b.push_char('d');
+        }
+        let p = Str(b.done());
+        assert_eq!(&p.as_ref()[0..10], "abdddddddd");
     }
 }
