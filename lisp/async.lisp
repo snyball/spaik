722c06320829093b0590bc909b3fb7ad7d655caa
Jonas Møller <jonas@moesys.no>
2023-04-04 01:59:58 +0200

    clean up in the lisp directory

--- lisp/async.lisp
+++
@@ -1,8 +0,0 @@
-(defmacro await (expr)
-  (let ((k (gensym)))
-    `(call/cc (lambda (,k)
-                (<ζ>::send-message ,expr ,k)
-                (throw '<ζ>::await)))))
-
-(defmacro send (expr)
-  `(<ζ>::send-message ,expr))
--- lisp/long_loop.lisp
+++
@@ -1,4 +0,0 @@
-(let ((x 1000000))
-  (loop (if (= x 0)
-            (break))
-        (set x (- x 1))))
