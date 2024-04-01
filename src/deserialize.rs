dace66828d594fb85b55981a27905bfc7c60cb86
Jonas Møller <jonas@moesys.no>
2024-04-01 16:46:28 +0200

    add RigidBody from rapier2d
    fix Body => RigidBody when rapier2d not enabled

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -70,6 +70,7 @@ impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
             PV::Bool(_) => self.deserialize_bool(visitor),
             PV::Char(_) => self.deserialize_char(visitor),
             PV::Id(_) => self.deserialize_u64(visitor),
+            PV::RigidBody(_) => self.deserialize_u64(visitor),
             #[cfg(feature = "math")]
             PV::Vec2(_) => self.deserialize_tuple(2, visitor),
             #[cfg(feature = "math")]
