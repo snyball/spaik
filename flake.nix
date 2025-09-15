bf5c866c14e7c007c59e2e239de78a10f002743f
Jonas Møller <jonas@moller.systems>
2025-09-15 21:45:45 +0200

    flakeify repo

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-xvsCckp+zh5HvVBArcMaKp7EpMV7i7wtXefh6UNWzcI=";
+        };
+        crane-lib = (crane.mkLib pkgs).overrideToolchain (p: toolchain);
+        wasm-tools = with pkgs; [
+          binaryen
+          twiggy
+          wabt
+          wasm-bindgen-cli
+        ];
+        build-tools = with pkgs; [ mold toolchain ];
+        all = build-tools ++ wasm-tools;
+      in
+        rec {
+          defaultPackage = crane-lib.buildPackage {
+            src = ./.;
+            nativeBuildInputs = build-tools;
+            doCheck = false;
+          };
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = all;
+          };
+        }
+    );
+}
