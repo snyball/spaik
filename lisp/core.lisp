827d549e5c64b6f899c2f542012905c1c3418a42
Jonas Møller <jonas@moller.systems>
2026-09-17 07:21:09 +0200

    core.lisp: remove unused code

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -416,15 +416,6 @@
     `(call/cc (lambda (,k)
                 (throw 'yield (cons ,expr ,k))))))
 
-(defmacro await (expr)
-  (let ((k (gensym)))
-    `(call/cc (lambda (,k)
-                (<ζ>-send-message ,expr ,k)
-                (throw 'yield '<ζ>-yield-await)))))
-
-(defmacro send (expr)
-  `(<ζ>-send-message ,expr))
-
 (defun nth (xs i &opt alt)
   (cond
    ((vec? xs) (if (< i (len xs))
